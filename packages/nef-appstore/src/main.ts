89e1eea55ca1e0f12c7e08181512ae8666bd7b89
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-07-15 16:42:09 +0200

    add CTA UI

--- packages/nef-appstore/src/main.ts
+++ packages/nef-appstore/src/main.ts
@@ -1,30 +1,11 @@
-import { GitHubAPI } from "./app/api/github"
-import { NefDocument } from "./app/utils/document"
-import { NefPlaygrounds } from "./app/badge-generator"
+import { CTADocument } from "./app/utils/document"
+import { NefCTA } from "./app/cta"
 import 'nef-common'
 
 function initPage() {
-  const githubClient = new GitHubAPI()
-  const dom = new NefDocument(document)
-  const component = new NefPlaygrounds(dom, githubClient)
-
-  addEventListeners(component, dom)
+  const dom = new CTADocument(document)
+  const component = new NefCTA(dom)
+  component.onStart()
 }
 
 initPage()
-
-// initialize
-function addEventListeners(component: NefPlaygrounds, dom: NefDocument) {
-  const field = dom.repositoryField()
-  const selector = dom.optionSelector()
-  const tagOption = dom.tagOption()
-  const branchOption = dom.branchOption()
-  const copyButton = dom.copyButton()
-
-  field?.addEnterKeyListener((element, event) => component.onRepositoryChanged(field, event))
-  field?.addEventListener("change", (event: Event) => component.onRepositoryChanged(field, event))
-  selector?.addEventListener("change", (event: Event) => component.onOptionSelected(selector, event))
-  tagOption?.addEventListener("click", (event: Event) => component.onSourceChanged(tagOption, event))
-  branchOption?.addEventListener("click", (event: Event) => component.onSourceChanged(branchOption, event))
-  copyButton?.addEventListener("click", (event: Event) => component.onCopyClicked(copyButton, event))
-}
